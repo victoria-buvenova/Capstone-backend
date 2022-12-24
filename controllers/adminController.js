d5844c18b139108e40c2724f0f5384f8016066d8
Victoria Buvenova <buvenova@gmail.com>
2022-12-24 17:56:56 +0700

    changed date-time adding

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,42 +1,39 @@
-const DBServices = require('../services/adminService');
+const DBServices = require("../services/adminService");
 
+const getAllClients = async (req, res) => {
+  let data = await DBServices.dbGetAllClients(req, res);
+  console.log(data);
+  res.send(data);
+};
 
-const getAllClients = async (req,res)=>{
-    let data = await DBServices.dbGetAllClients(req,res)
-    console.log(data)
-    res.send(data)
+const addMyTime = async (req, res) => {
+  let data = await DBServices.dbAddMyTime(req, res);
+  console.log(data);
+  res.send("Successfully added time");
+};
 
-}
+const updateDate = async (req, res) => {
+  let data = await DBServices.dbUpdateDateById(req, res);
+  console.log(data);
+  res.send("Successfully updated date");
+};
 
-const addMyTime = async (req,res)=>{
-    let data = await DBServices.dbAddMyTime(req,res)
-    console.log(data)
-    res.send('Successfully added time')
+// const updateTime = async (req,res)=>{
+//     let data = await DBServices.dbUpdateTimeById(req,res)
+//     console.log(data)
+//     res.send('Successfully updated time')
+// }
 
-}
-
-const updateDate = async (req,res)=>{
-    let data = await DBServices.dbUpdateDateById(req,res)
-    console.log(data)
-    res.send('Successfully updated date')
-}
-
-const updateTime = async (req,res)=>{
-    let data = await DBServices.dbUpdateTimeById(req,res)
-    console.log(data)
-    res.send('Successfully updated time')
-}
-
-const deleteById = async (req,res)=>{
-    let data = await DBServices.dbDeleteById(req,res)
-    console.log(data)
-    res.send('Successfully deleted')
-}
+const deleteById = async (req, res) => {
+  let data = await DBServices.dbDeleteById(req, res);
+  console.log(data);
+  res.send("Successfully deleted");
+};
 
 module.exports = {
-    getAllClients,
-    addMyTime,
-    updateTime,
-    updateDate,
-    deleteById
-}
+  getAllClients,
+  addMyTime,
+  // updateTime,
+  updateDate,
+  deleteById,
+};
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,28 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const adminController = require('../controllers/adminController') 
+const adminController = require("../controllers/adminController");
 
-router.get('/getall' ,(req,res)=>{
-    adminController.getAllClients(req,res)
+router.get("/getall", (req, res) => {
+  adminController.getAllClients(req, res);
 });
 
 // this is for the admin to add time
-router.post('/add' ,(req,res)=>{
-    adminController.addMyTime(req,res)
+router.post("/add", (req, res) => {
+  adminController.addMyTime(req, res);
 });
 
+router.put("/updatedateandtime", (req, res) => {
+  adminController.updateDate(req, res);
+});
 
-router.put('/updatedate',(req,res)=>{
-    adminController.updateDate(req,res)
-})
+// router.put('/updatetime',(req,res)=>{
+//     adminController.updateTime(req,res)
+// })
 
-router.put('/updatetime',(req,res)=>{
-    adminController.updateTime(req,res)
-})
-
-router.delete('/deletebyid',(req,res)=>{
-    adminController.deleteById(req,res)
-})
+router.delete("/deletebyid", (req, res) => {
+  adminController.deleteById(req, res);
+});
 
 module.exports = router;
